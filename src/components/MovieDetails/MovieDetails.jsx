548d467b49fc72f09fbd563b3bda6a868f45e452
Ryan Weispfenning <weisp014@gmail.com>
2023-04-09 11:21:09 -0500

    added back button to details page

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,12 +1,19 @@
 import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 
 function MovieDetails() {
   const movie = useSelector((store) => store.movieDetails);
+  const history = useHistory();
   console.log(movie);
+
+  const backHandler = () => {
+    history.goBack();
+  }
 
   return (
     <>
       <div id="movie_details" key={movie.id}>
+        <button onClick={backHandler}>BACK</button>
         <h3>{movie.title}</h3>
         <img src={movie[0].poster} alt={movie[0].title} />
         <h5>Description: {movie[0].description}</h5>
